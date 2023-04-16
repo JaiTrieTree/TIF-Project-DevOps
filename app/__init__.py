b0b572854b370ada854d82250fdb14366065cb80
root <root@Jai.localdomain>
2023-04-16 21:53:21 +0530

    changes in DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,41 @@
+#from flask import Flask
+#import mysql.connector
+
+#app = Flask(__name__)
+
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'teluguitfactory'
+#app.config['MYSQL_DB'] = 'teluguitfactorydb'
+
+#mysql = mysql.connector.connect(
+#    host=app.config['MYSQL_HOST'],
+#    user=app.config['MYSQL_USER'],
+#    password=app.config['MYSQL_PASSWORD'],
+#    database=app.config['MYSQL_DB']
+#)
+
+#from app import course, login, cart, price_list
 from flask import Flask
-import mysql.connector
+from flask_mysqldb import MySQL
+from flask import session
 
 app = Flask(__name__)
 
+# Update these placeholders with your MySQL credentials and database name
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'teluguitfactory'
 app.config['MYSQL_PASSWORD'] = 'teluguitfactory'
 app.config['MYSQL_DB'] = 'teluguitfactorydb'
 
-mysql = mysql.connector.connect(
-    host=app.config['MYSQL_HOST'],
-    user=app.config['MYSQL_USER'],
-    password=app.config['MYSQL_PASSWORD'],
-    database=app.config['MYSQL_DB']
-)
+# Add a secret key for session support
+app.secret_key = 'teluguitfactory'
+
+mysql = MySQL(app)
 
 from app import course, login, cart, price_list
 
+# Redirect the root URL to the login page
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
